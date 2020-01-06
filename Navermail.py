fd5dca8fb9112b281f0481b0e1a652f8932c2b30
greencodetree <58033423+greencodetree@users.noreply.github.com>
2020-01-06 22:58:12 +0900

    Added Navermail.py
    Instead of gmail Navermail.py is for sending email from Naver.

---
+++ Navermail.py
@@ -0,0 +1,23 @@
+import smtplib
+from email.mime.text import MIMEText
+
+sendEmail = ""    #보내는 네이버 이메일 주소
+recvEmail = ""       #받는 이메일 주소
+password = ""             #네이버 비밀번호
+
+smtpName = "smtp.naver.com" #smtp 서버 주소
+smtpPort = 587 #smtp 포트 번호
+
+text = "This is a test mail"    #메일 내용
+msg = MIMEText(text) #MIMEText(text , _charset = "utf8")
+
+msg['Subject'] ="Test mail" #메일 제목
+msg['From'] = sendEmail 
+msg['To'] = recvEmail
+print(msg.as_string()) #전송을 위해 전용함수 as_string 으로 텍스트 형태로 변환
+
+s=smtplib.SMTP( smtpName , smtpPort ) #메일 서버 연결
+s.starttls() #TLS 보안 처리
+s.login( sendEmail , password ) #로그인
+s.sendmail( sendEmail, recvEmail, msg.as_string() ) #메일 전송, 문자열로 변환해야 합니다.
+s.close() #smtp 서버 연결을 종료합니다.
